2fd42852d28673f5b88b138932ed82b35a1a4016
chicoreus <mole@morris.net>
2023-06-07 15:57:27 -0400

    Adding csv export of the v42 data validation sheet, and loading it for conversion to to TG2_test_validation_data.csv in TestOfTestSpreadsheetUtility test validation data as of 2023-06-05 (note, run to convert does find errors)

--- src/main/java/org/filteredpush/qc/bdqtestrunner/TestOfTestSpreasheetUtility.java
+++ src/main/java/org/filteredpush/qc/bdqtestrunner/TestOfTestSpreasheetUtility.java
@@ -181,6 +181,7 @@ public class TestOfTestSpreasheetUtility {
 	    filename = "/Test_data_32_2022-09-11.csv";
 	    filename = "/Test_data_33_2022-09-12.csv";
 	    filename = "/Test_data_35_2022-11-11.csv";
+	    filename = "/Test_data_42_2023-03-30.csv";
 	    URL urlinfile = TestOfTestSpreasheetUtility.class.getResource(filename);
 	    File inputfile = new File(urlinfile.toURI());
 	    Reader in = new FileReader(inputfile);
