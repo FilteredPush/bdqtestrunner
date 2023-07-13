17f80c68ea8db0fd5411f61c061aeba921b6e13c
chicoreus <mole@morris.net>
2023-07-13 12:21:09 -0400

    Adding support for tests with more parameters (to support superfamily, tribe, subfamily parameters), adding support for scientific name authority class as a parameter for ammendments with three parameters.

--- src/main/java/org/filteredpush/qc/bdqtestrunner/TestRunner.java
+++ src/main/java/org/filteredpush/qc/bdqtestrunner/TestRunner.java
@@ -646,6 +646,62 @@ public class TestRunner {
 										paramValues.get(21), 
 										paramValues.get(22), 
 										paramValues.get(23));
+							} else if (paramValues.size()==25 && javaMethod.getParameterCount()==25) { 
+								retval = (DQResponse<ComplianceValue>)javaMethod.invoke(instance, 
+										paramValues.get(0), 
+										paramValues.get(1), 
+										paramValues.get(2), 
+										paramValues.get(3), 
+										paramValues.get(4), 
+										paramValues.get(5), 
+										paramValues.get(6), 
+										paramValues.get(7), 
+										paramValues.get(8), 
+										paramValues.get(9), 
+										paramValues.get(10), 
+										paramValues.get(11), 
+										paramValues.get(12), 
+										paramValues.get(13), 
+										paramValues.get(14), 
+										paramValues.get(15), 
+										paramValues.get(16), 
+										paramValues.get(17), 
+										paramValues.get(18), 
+										paramValues.get(19), 
+										paramValues.get(20), 
+										paramValues.get(21), 
+										paramValues.get(22), 
+										paramValues.get(23), 
+										paramValues.get(24));								
+							} else if (paramValues.size()==27 && javaMethod.getParameterCount()==27) { 
+								retval = (DQResponse<ComplianceValue>)javaMethod.invoke(instance, 
+										paramValues.get(0), 
+										paramValues.get(1), 
+										paramValues.get(2), 
+										paramValues.get(3), 
+										paramValues.get(4), 
+										paramValues.get(5), 
+										paramValues.get(6), 
+										paramValues.get(7), 
+										paramValues.get(8), 
+										paramValues.get(9), 
+										paramValues.get(10), 
+										paramValues.get(11), 
+										paramValues.get(12), 
+										paramValues.get(13), 
+										paramValues.get(14), 
+										paramValues.get(15), 
+										paramValues.get(16), 
+										paramValues.get(17), 
+										paramValues.get(18), 
+										paramValues.get(19), 
+										paramValues.get(20), 
+										paramValues.get(21), 
+										paramValues.get(22), 
+										paramValues.get(23), 
+										paramValues.get(24), 
+										paramValues.get(25), 
+										paramValues.get(26));								
 							} else { 
 								logger.error("No implementation of invocation with needed number of parameters " + Integer.toString(paramValues.size()) + " for " + GUID );
 							}
@@ -707,7 +763,18 @@ public class TestRunner {
 								} else if (paramValues.size()==2 && javaMethod.getParameterCount()==2) { 
 									retval = (DQResponse<AmendmentValue>)javaMethod.invoke(instance, paramValues.get(0), paramValues.get(1));
 								} else if (paramValues.size()==3 && javaMethod.getParameterCount()==3) { 
-									retval = (DQResponse<AmendmentValue>)javaMethod.invoke(instance, paramValues.get(0), paramValues.get(1), paramValues.get(2));
+									if (javaMethod.getParameters()[2].getType().equals(SciNameSourceAuthority.class)) {
+										SciNameSourceAuthority replacementParam = new SciNameSourceAuthority();
+										if (DateUtils.isEmpty(paramValues.get(2))) { 
+											// pass null as parameter to select default source authority.
+											replacementParam = null;
+										} else {
+											replacementParam = new SciNameSourceAuthority(paramValues.get(2));
+										}
+										retval = (DQResponse<AmendmentValue>)javaMethod.invoke(instance, paramValues.get(0), paramValues.get(1), replacementParam);
+									} else { 
+										retval = (DQResponse<AmendmentValue>)javaMethod.invoke(instance, paramValues.get(0), paramValues.get(1), paramValues.get(2));
+									}
 								} else if (paramValues.size()==4 && javaMethod.getParameterCount()==4) { 
 									retval = (DQResponse<AmendmentValue>)javaMethod.invoke(instance, paramValues.get(0), paramValues.get(1), paramValues.get(2), paramValues.get(3));
 								} else if (paramValues.size()==5 && javaMethod.getParameterCount()==5) { 
