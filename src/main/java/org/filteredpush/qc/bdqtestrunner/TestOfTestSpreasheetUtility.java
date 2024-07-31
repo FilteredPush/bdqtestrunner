2990aeb12088ce87d00474edf138dbcf5682323f
chicoreus <mole@morris.net>
2024-07-31 08:03:48 -0400

    Adding validation data sheet export v75 from @Tasilee as Test_data_75_2024_07_31.csv

--- src/main/java/org/filteredpush/qc/bdqtestrunner/TestOfTestSpreasheetUtility.java
+++ src/main/java/org/filteredpush/qc/bdqtestrunner/TestOfTestSpreasheetUtility.java
@@ -224,6 +224,7 @@ public class TestOfTestSpreasheetUtility {
 	    filename = "/Test_data_72_2024_07_25.csv";
 	    filename = "/Test_data_73_2024_07_30.csv";
 	    filename = "/Test_data_74_2024_07_30.csv";
+	    filename = "/Test_data_75_2024_07_31.csv";
 	    //URL urlinfile = TestOfTestSpreasheetUtility.class.getResource(filename);
 	    //File inputfile = new File(urlinfile.toURI());
 	    //Reader in = new FileReader(inputfile);
